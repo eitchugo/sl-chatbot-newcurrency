61d69787f1c4bb011d4dab5568e936fed6e5cd01
Hugo Cisneiros (Eitch) <hugo.cisneiros@gmail.com>
2019-02-23 18:52:18 -0300

    initial working version

---
+++ NewCurrency_StreamlabsSystem.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Allow subs to create commands."""
+
+# system libraries
+import os
+import sys
+
+# application libraries
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+from settings import MySettings  # noqa: E402
+from database import InstancedDatabase  # noqa: E402
+from currency import Currency  # noqa: E402
+
+# [Required] Script Information
+ScriptName = 'NewCurrency'
+Website = 'https://twitch.tv/eitch'
+Description = 'Adds a new currency to your channel, independent of StreamLabs builtin one.'
+Creator = 'Eitch'
+Version = '0.5.0'
+
+# Define Global Variables
+database_file = os.path.join(os.path.dirname(__file__), 'Currency.db')
+config_file = os.path.join(os.path.dirname(__file__), 'settings.json')
+
+
+def Init():
+    """ [Required] Initialize Data (Only called on load) """
+
+    # settings initialization
+    global settings
+    settings = MySettings(config_file)
+    settings.save()
+
+    # database initialization
+    db = InstancedDatabase(database_file)
+    global currency
+    currency = Currency(Parent, db, settings.name, settings.frequency*60, settings.quantity)
+
+    return
+
+
+def Execute(data):
+    """ [Required] Execute Data / Process messages """
+    return
+
+
+def Tick():
+    """ [Required] Tick method (Gets called during every iteration even when there is no incoming data) """
+    if Parent.IsLive():
+        # always gain currency while live
+        currency.start_timer()
+    else:
+        # gain currency while offline, if setting is enabled
+        if settings.gain_offline:
+            currency.start_timer()
+        else:
+            # offline, disable currency gain
+            currency.stop_timer()
+
+    return
+
+
+def ReloadSettings(json_data):
+    """ [Optional] Reload Settings (Called when a user clicks the Save Settings button in the Chatbot UI) """
+    settings.reload(json_data)
+    settings.save()
+    currency.name = settings.name
+    currency.frequency = settings.frequency
+    currency.quantity = settings.quantity
+    return
+
+
+def Unload():
+    """ [Optional] Unload (Called when a user reloads their scripts or closes the bot / cleanup stuff) """
+    currency.stop_timer()
+    return
+
+
+def ScriptToggled(state):
+    """ [Optional] ScriptToggled (Notifies you when a user disables your script or enables it) """
+    return
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# sl-chatbot-newcurrency
+# StreamLabs Chatbot Script - New Currency
+
 Script for Streamlabs Chatbot for adding a new currency
+
+# Installation and use
+
+On Streamlabs Chatbot interface, go to `Scripts` and on the top right icons,
+click on the `Import` button and chose the zip file containing this
+repository. This will install the script itself.
+
+# Database
+
+The script uses SQLite python library to create a local database file containing
+all the users and their currencies. The database file location is the same as the
+script itself. For example:
+
+* C:\Users\`<USER>`\AppData\Roaming\Streamlabs\Streamlabs Chatbot\Services\Scripts\sl-chatbot-newcurrency
+
+Where `<USER>` is your local user. All databases used by this script will have
+the `.db` extension.
+
+# Change Log
+
+## 0.5.0
+
+* Initial public release
+
+# Author
+
+* Hugo Cisneiros (Eitch)
+* Website: https://twitch.tv/eitch
